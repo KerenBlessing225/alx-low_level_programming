56dd4df97141e63a6240f52084a0c238e507129b
KerenBlessing225 <e.mousso@yahoo.com>
2022-07-08 03:09:30 -0700

    Write a function that checks for a digit (0 through 9). Prototype: int _isdigit(int c); Returns 1 if c is a digit Returns 0 otherwise FYI: The standard library provides a similar function: isdigit. Run man isdigit to learn more.

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,7 +4,7 @@
  * _isdigit - a function that checks for a digit ( 0 through 9)
  * Return: 1 if c is a digit, 0 otherwise
  */
-int _isupper(int c)
+int _isdigit(int c)
 {
 	if ((c >= 0) && (c <= 9))
 		return (1);
