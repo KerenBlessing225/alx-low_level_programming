3793fc772bd6d182e1680bd492c0c1a0c00173f3
KerenBlessing225 <e.mousso@yahoo.com>
2022-07-23 12:54:37 -0700

    Write a program that prints its name, followed by a new line. If you rename the program, it will print the new name, without having to compile it again. You should not remove the path before the name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a program that prints its name
+ * @argc: the number of arguments typed in the command line
+ * @argv: array that points to the arguments
+ * Return: 0
+ */
+int main (int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
