122ae751ad70ca44378492f44a8cc2509061f1ab
KerenBlessing225 <e.mousso@yahoo.com>
2022-07-23 14:14:18 -0700

    Write a program that adds positive numbers. Print the result, followed by a new line. If no number is passed to the program, print 0, followed by a new line. If one of the number contains symbols that are not digits, print Error, followed by a new line, and return 1. You can assume that numbers and the addition of all the numbers can be stored in an int.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,14 +8,13 @@
  * @argv: array pointing to arguements
  * Return: 0
  */
-int main(int argc, __attribute__((unused)) char *argv[])
+int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
 	if (argc < 1)
-	{
 		return (0);
-	}
+
 	for (i = 1; i < argc; i++)
 	{
 		if (!atoi(argv[i]))
